43f0f7d7a5893904ec1ef08115d8eb07f902949d
ATAALIALLAHWANI <ataahwani007@gmail.com>
2026-09-24 21:51:19 +0300

    Add CrossOrigin to HomeController so / and /api/ping work from a different-origin frontend

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Map;
  * (The existing TutorialController under /api/tutorials needs MongoDB; these do not.)
  */
 @RestController
+@CrossOrigin(origins = "*")
 public class HomeController {
 
     @GetMapping("/")
